d839dd9a9d7942cec5b92a00358a282ba0931a06
Thiago_Batista_da_Silva_Oliveira <gmoliveira69@gmail.com>
2023-12-15 15:47:13 -0300

    feat: plus icon

--- src/screens/Home/Home.tsx
+++ src/screens/Home/Home.tsx
@@ -2,6 +2,7 @@ import { Header } from "@components/Header";
 import { useNavigation } from "@react-navigation/native"
 import { Container } from "./styles";
 import { Avaliation } from "./components/Avaliation";
+import { Meals } from "./components/Meals";
 
 export const Home = ()=> {
     const navigation = useNavigation();
@@ -18,6 +19,7 @@ export const Home = ()=> {
         <Container>
            <Header />
            <Avaliation avaliation={90} onPressNavigate={() => handleDetail() } />
+           <Meals />
         </Container>
     )
 }
--- src/screens/Home/components/Meals/index.tsx
+++ src/screens/Home/components/Meals/index.tsx
@@ -1,11 +1,14 @@
 import { Button } from "@components/Button"
 import { ComponentTitle, Container } from "./styles"
+import { Plus } from "phosphor-react-native"
 
 export const Meals = () => {
     return (
         <Container>
-         <ComponentTitle>Refeições</ComponentTitle>
-         <Button onPress={() => console.log('clicou')} title="Nova Refeição" />
+            <ComponentTitle>Refeições</ComponentTitle>
+            <Button 
+              Icon={<Plus color="#FFFFFF" size={24} />}
+              onPress={() => console.log('clicou')} title="Nova Refeição" />
         </Container>
     )
 }
