e409544501cedbb0563f300b01080009dcd82ac7
Thiago_Batista_da_Silva_Oliveira <gmoliveira69@gmail.com>
2024-01-09 11:18:59 -0300

    feat: Controlled input

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,16 +1,21 @@
+import { Control, Controller, FieldValues, useForm } from "react-hook-form";
 import { Container, StyledInput, Title } from "./styles";
 
 interface IProps {
     title: string;
+    name: string;
+    control: Control<FieldValues, any>;
     placeholder?: string;
     height?: string;
 }
 
-export const Input = ({title, placeholder, height = '48px'}: IProps) => {
+export const ControlledInput = ({title, name, control, placeholder, height = '48px'}: IProps) => {
     return (
+       <Controller name={name} control={control} render={({field: {onChange, value}}) => (
         <Container>
-         <Title>{title}</Title>
-         <StyledInput height={height} placeholder={placeholder}/>
-        </Container>
+        <Title>{title}</Title>
+        <StyledInput onChange={onChange} value={value} height={height} placeholder={placeholder}/>
+       </Container>
+    )} />
     )
 }
